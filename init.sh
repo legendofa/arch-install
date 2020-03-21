fa8173854a9e7c60cdaf157f9feaee6f25df240c
st170001 <st170001@stud.uni-stuttgart.de>
2020-03-21 16:09:11 +0100

    Improved setup automation

--- init.sh
+++ init.sh
@@ -2,14 +2,9 @@
 
 # Initialize the installation
 lsblk
-mkdir /usb
-mount /dev/sda1 /usb
 pacman -Sy
 pacman -S git --noconfirm
-git clone http://192.168.178.67:8080/Julian/arch-install /usb
-git clone http://192.168.178.67:8080/Julian/Julian /usb
-git pull http://192.168.178.67:8080/Julian/arch-install /usb
-git pull http://192.168.178.67:8080/Julian/Julian /usb
+git clone http://192.168.178.67:8080/Julian/arch-install
+git clone http://192.168.178.67:8080/Julian/Julian
 pacman -Rns git --noconfirm
-cd /usb
 ls -la
