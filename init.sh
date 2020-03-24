16247da2bbbd476241c22db35bc423c34e3fea92
st170001 <st170001@stud.uni-stuttgart.de>
2020-03-24 22:00:40 +0100

    init.sh update

--- init.sh
+++ init.sh
@@ -3,7 +3,6 @@
 # Initialize the installation
 lsblk
 pacman -Sy
-pacman -S git --noconfirm
-git clone http://192.168.178.67:8080/Julian/arch-install
-pacman -Rns git --noconfirm
+pacman -S git --noconfirm && cd arch-install && git pull
+# git clone http://192.168.178.67:8080/Julian/arch-install
 ls -la
