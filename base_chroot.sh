04fd998adc44a6a811458188fb1dadea6fc02f7c
st170001 <st170001@stud.uni-stuttgart.de>
2020-03-26 13:25:12 +0100

    Directory existence check

--- base_chroot.sh
+++ base_chroot.sh
@@ -69,7 +69,7 @@ systembeep_off
 aur_package_install $USERNAME $AURHELPER $DISPLAY_MANAGER
 
 # Install user configurations
-_ mkdir /temp
+_ mkdir -p /temp
 _ git clone $CONFIG_FILES /temp
 _ chown -R $USERNAME /temp
 _ chgrp -R wheel /temp
